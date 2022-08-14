0c4a1474ffe3b027f4426d179b2ca1c55ce7303b
nan <nan>
2022-08-14 19:57:24 +0800

    first commit

---
+++ verify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+pip3 install -i https://pypi.doubanio.com/simple adapter-transformers flwr & 
+git clone https://github.com/adap/flower && cd flower 
+
+cd examples/quickstart_huggingface && bash run.sh
+
+
