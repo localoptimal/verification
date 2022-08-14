aa5f1b56a05d6149e80ec2512c351ef6a97b8282
submartingales <100008553+submartingales@users.noreply.github.com>
2022-08-15 05:32:56 +0800

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-pip3 install -i https://pypi.doubanio.com/simple adapter-transformers flwr & 
+pip3 install -i https://pypi.doubanio.com/simple adapter-transformers datasets flwr & 
 git clone https://github.com/adap/flower && cd flower 
 
 cd examples/quickstart_huggingface && bash run.sh
